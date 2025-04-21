e584fb5faae00ecd3304dfbdae28b3a518ad8503
saman19910 <samannori058@gmail.com>
2025-04-21 12:40:56 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import ChatPage from "./pages/ChatPage";
-import Dashboard from "./pages/Dashboard";
-import ImageGeneratorPage from "./pages/ImageGeneratorPage";
-import AboutPage from "./pages/AboutPage";
+import HomePage from "./HomePage";
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/image-generator" element={<ImageGeneratorPage />} />
-        <Route path="/about" element={<AboutPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </Router>
   );
 }
-
-export default App;
